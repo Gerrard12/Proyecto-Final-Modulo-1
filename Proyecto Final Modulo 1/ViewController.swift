2c8e00c821e547b6a6002564aa65cb9f94459f27
Gerardo Sanchez <gerardo@killaribyte.com>
2017-02-20 12:47:04 -0500

    Initial Commit

---
+++ Proyecto Final Modulo 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Proyecto Final Modulo 1
+//
+//  Created by Gerardo Sanchez Calderon  on 20/02/17.
+//  Copyright © 2017 Gerardo Sanchez Calderon . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
